e4ceba5d2ba427d39b2bddd782357520e24f1179
Manuel Alejandro Guarnizo <manuel.guarnizo-g@mail.escuelaing.edu.co>
2025-09-10 17:33:51 -0500

    patrón observado y aplicación principal con springboot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # taller1DOSW
 taller evaluativo primer tercio dosw
+
+
+El patrón que se implementa es el Observer porque el 'ProductService'}
+notifica a los observadores del Stock (StockObserver) cuando cambian
+los precios.
---
+++ src/main/java/edu/dosw/taller1dosw/Taller1DoswApplication.java
@@ -0,0 +1,11 @@
+package edu.dosw.taller1dosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1DoswApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Taller1DoswApplication.class, args);
+    }
+}
