8cfb2ad5a6bc2df9582f42198abf0bfb7bc77e3a
Manuel Alejandro Guarnizo <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-09-11 14:23:35 -0500

    feature: aumento cobertura rama test AgenteAdvertencia

--- src/test/java/AgenteAdvertenciaTest.java
+++ src/test/java/AgenteAdvertenciaTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AgenteAdvertenciaTest {
@@ -25,4 +26,20 @@ class AgenteAdvertenciaTest {
         String texto = salida.toString();
         assertTrue(texto.contains("ALERTA"));
     }
+    @Test
+    void noDebeImprimirAlertaCuandoStockEsAlto() {
+        AgenteAdvertencia agente = new AgenteAdvertencia();
+        Producto p = new Producto("Goma", "Papeleria", 1.0, 6);
+
+        ByteArrayOutputStream salida = new ByteArrayOutputStream();
+        PrintStream original = System.out;
+        System.setOut(new PrintStream(salida));
+
+        agente.cambiosEnStock(p);
+
+        System.setOut(original);
+        String texto = salida.toString();
+        assertFalse(texto.contains("ALERTA"));
+    }
+
 }
